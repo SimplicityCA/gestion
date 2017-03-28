a1b3dac6dc842e39370fb51c0ddf6c9f8920aff3
Joshua Fabara <joshuafabara@gmail.com>
2017-03-28 11:52:52 -0500

    Drupal 8 core

---
+++ sites/default/files/php/twig/6a16564d_input.html.twig_28664dbe64a03a9a6819cd41a97d2e6b48f381e4a46bd6fc2590da3b3abb3202/79a0f7a75dce3ab390045c32e7c050d72fabf657f07912e7543be920cdbef370.php
@@ -0,0 +1,79 @@
+<?php
+
+/* core/themes/classy/templates/form/input.html.twig */
+class __TwigTemplate_61e799c6ac18aabead0f9e6825bb8691f9e726cb083a3488322a3539d08518db extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 13
+        echo "<input";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["attributes"]) ? $context["attributes"] : null), "html", null, true));
+        echo " />";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["children"]) ? $context["children"] : null), "html", null, true));
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/classy/templates/form/input.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 13,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override for an 'input' #type form element.*/
+/*  **/
+/*  * Available variables:*/
+/*  * - attributes: A list of HTML attributes for the input element.*/
+/*  * - children: Optional additional rendered elements.*/
+/*  **/
+/*  * @see template_preprocess_input()*/
+/*  *//* */
+/* #}*/
+/* <input{{ attributes }} />{{ children }}*/
+/* */
