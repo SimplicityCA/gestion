8627a56da78ee5d6eb2cde6a3ad524810ee37dbf
joserhond <joserhond@hotmail.com>
2017-06-09 16:38:17 -0400

    create mixins and variables files, add styles in menu and bar in slider according to tag

--- themes/gestion/js/main.js
+++ themes/gestion/js/main.js
@@ -6,4 +6,16 @@ jQuery( document ).ready(function() {
       return false;
     });
   }
+  var tags = [];
+  var i = 0;
+  jQuery("#homePageCarousel .node--type-article .teaser-field-group .field--name-field-tags > .field--item > div > h2 > a").each(function(){
+  	if(jQuery(this).attr('href')!= "/analisis"){
+  		//jQuery(".field--name-field-autor").addClass(jQuery(this).attr('href').substring(1));
+  		tags[i++] = jQuery(this).attr('href').substring(1);
+  	}
+  });
+  i = 0;
+  jQuery("#homePageCarousel .node--type-article .teaser-field-group .title-and-author-wrapper .field--name-field-autor").each(function(){
+  	jQuery(this).addClass(tags[i++]);
+  });
 });
